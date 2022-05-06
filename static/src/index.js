e4fb34bc20cfa7ad7f64c1db3f2364d70ba5a2ab
EC2 Default User <ec2-user@ip-10-0-12-195.ec2.internal>
2022-05-06 05:36:57 +0000

    work?

--- static/src/index.js
+++ static/src/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(
-//  <React.StrictMode>
-//    <App />
-//  </React.StrictMode>
-//);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Search />
+  </React.StrictMode>
+);
 
 export default class Search extends React.Component {
     constructor(props) {
