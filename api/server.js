537be58fc2b30f3b732f10b3a1335f0a0adde40c
EC2 Default User <ec2-user@ip-10-0-12-195.ec2.internal>
2022-05-08 22:57:13 +0000

    Whitescreen

--- api/server.js
+++ api/server.js
@@ -24,13 +24,21 @@ let httpssl = https.createServer(
 	app.callback()
 );
 
-console.log("Server is listening.");
 
 app.use( parser() );
 app.use( cors() );
 app.use( log() );
 
 const router = new Router();
+app.use( router.routes() );
+//app.listen(3000);
+httpssl.listen(443, err => {if (err) console.log(err); });
+console.log("Server is listening.");
+
+router.get("/", async (ctx, next) => {
+	console.log("Get received.");
+});
+
 router.post("/query", async (ctx, next) => {
 	const client = new Client({
 		user: 'Flamdini',
@@ -43,116 +51,129 @@ router.post("/query", async (ctx, next) => {
 	client.connect();
 	const jstring = JSON.stringify(ctx.request.body);
 	const json = JSON.parse(jstring);
-	const basetable= queryconstruct(json);
-	const qandatable = "CREATE TEMP TABLE MyQuery2 AS SELECT PostTypeId, ParentOrChild, COUNT(*) FROM MyQuery GROUP BY PostTypeId, ParentOrChild;";
-	const qandaquery = `SELECT * FROM MyQuery2;`
-	const totalquery = "SELECT PostTypeId, COUNT(*) FROM MyQuery2 GROUP BY PostTypeId;";
-	const viewquery = "SELECT ViewCount, COUNT(*) FROM MyQuery GROUP BY ViewCount;";
-	const scorequery = "SELECT Score, COUNT(*) FROM MyQuery GROUP BY Score;";
-	const datequery = "SELECT year, month, COUNT(*) FROM MyQuery GROUP BY year, month;";
-	console.log("Timed out?");
-	const [r1, r2, r3, r4, r5, r6] = await Promise.all([
-		client.query(basetable),
-		client.query(qandatable),
-		client.query(qandaquery),
-		client.query(totalquery),
-		client.query(viewquery),
-		client.query(scorequery),
-		client.query(datequery)
-	]);
-	console.log("Timed out?");
-	console.log(r3, r4, r5, r6);
-	app.use(async ctx => {
-			res.header('Access-Control-Allow-Origin', '*');
-			ctx.body = [r3, r4, r5, r6];
-	});
+	const basetable = queryConstruct(json);
+	const qandatable = `CREATE TEMP TABLE MyQuery2 AS (SELECT PostTypeId, ParentOrChild, COUNT(*) AS answers FROM MyQuery GROUP BY ParentOrChild, PostTypeId);`;
+	const qandaquery = {
+		text: `SELECT answers, COUNT(*) FROM MyQuery2 WHERE PostTypeId = 2 GROUP BY answers ORDER BY answers;`,
+		rowMode: `array`
+	};
+	const viewquery = {
+		text: `SELECT ViewCount, COUNT(*) FROM MyQuery WHERE ViewCount IS NOT NULL GROUP BY ViewCount ORDER BY ViewCount;`,
+		rowMode: `array`
+	};
+	const unsatquery = {
+		text: `SELECT COUNT(*) FROM MyQuery WHERE PostTypeId = 1 AND ParentOrChild IS NULL;`,
+		rowMode: `array`
+	};
+	const astotquery = {
+		text: `SELECT year, month, COUNT(*) FROM MyQuery WHERE PostTypeId = 1 AND Id NOT IN (SELECT ParentOrChild FROM MyQuery WHERE PostTypeId = 2) GROUP BY year, month ORDER BY year, month;`,
+		rowMode: `array`
+	};
+	const totalquery = {
+		text: `SELECT PostTypeId, COUNT(PostTypeId) FROM MyQuery GROUP BY PostTypeId ORDER BY PostTypeId;`,
+		rowMode: `array`
+	};
+	const scorequery = {
+		text: `SELECT Score, COUNT(*) FROM MyQuery GROUP BY Score ORDER BY Score;`,
+		rowMode: `array`
+	};
+	const datequery = {
+		text: `SELECT year, month, COUNT(*) FROM MyQuery GROUP BY year, month ORDER BY year, month;`,
+		rowMode: `array`
+	};
+	console.log("Forming temporary table.");
+	console.log(basetable);
+	await client.query(basetable);
+	console.log("Temporary table formed.");
+	if(json.advsearch == true){
+		const res = [];
+		if(json.includequestion == true){
+			await client.query(qandatable);
+			res.push(await client.query(qandaquery));
+			res.push(await client.query(viewquery));
+			if(json.includeunsatisfied == true){
+				res.push(await client.query(unsatquery));
+			}
+		}
+		if(json.includeanswer == true && json.includeunsatisfied == true){
+			res.push(await client.query(astotquery));
+		}
+	}
+	res.push(await client.query(totalquery));
+	res.push(await client.query(scorequery));
+	res.push(await client.query(datequery));
+	console.log(res);
+	ctx.body = res;
+	console.log(await client.query(`SELECT * FROM MyQuery;`));
 });
 
-router.get("/", async (ctx, next) => {
-	console.log("Get received.");
-});
+function queryConstruct(json){
 
-app.use( router.routes() );
+	const viewsmin = parseInt(json.viewsmin) || "-2147483647";
+	const viewsmax = parseInt(json.viewsmax) || "2147483647";
+	const scoremin = parseInt(json.scoremin) || "-2147483647";
+	const scoremax = parseInt(json.scoremax) || "2147483647";
+	const datemin = json.datemin == "" ? "0001-01-01T00:00:00" : json.datemin;
+	const datemax = json.datemax == "" ? "9999-12-12T23:59:59" : json.datemax;
 
-function queryconstruct(json){
+	const table = json.table;
 
-	const viewsmin = parseInt(json.viewsmin) || null;
-	const viewsmax = parseInt(json.viewsmax) || null;
-	const scoremin = parseInt(json.scoremin) || null;
-	const scoremax = parseInt(json.scoremax) || null;
-	const datemin = json.datemin == "" ? null : json.datemin;
-	const datemax = json.datemax == "" ? null : json.datemax;
+	const qastring = checkInjector(json, table);
+	const viewstring = viewgroupInjector();
 
 	const titlestring = (json.title != "") ? fieldInjector(json.title.split(" "), "Title", json.includequestion, json.includeanswer) : "";
 	const bodystring = (json.body != "") ? fieldInjector(json.body.split(" "), "Body", json.includequestion, json.includeanswer) : "";
 	const tagstring = (json.tags != "") ? fieldInjector(json.tags.split(" "), "Tags", json.includequestion, json.includeanswer) : "";
-	const viewstring = rangeInjector(json.includequestion, json.includeanswer, viewsmin, viewsmax);
-	const qstring = checkInjector(json.includequestion, json.includesatisfied, json.includeunsatisfied, 1);
-	const astring = checkInjector(json.includeanswer, json.includeaccepted, json.includeother, 2);
 
 	let querystring = `CREATE TEMP TABLE MyQuery AS ` + 
 						`SELECT PostTypeId, ` +
-								`EXTRACT(YEAR FROM CreationDate) AS year, ` +
-								`EXTRACT(MONTH FROM CreationDate) AS month, ` +
-								`Id, ` +
-								`ParentOrChild, ` +
-								`Score, ` +
-								`ViewCount ` +
-						`FROM ${json.table} ` +
-						`WHERE (${qstring} OR ${astring}) ` +
-							`AND ((CreationDate BETWEEN ${datemin} AND ${datemax}) ` +
-												`OR (${datemin} IS NULL AND ${datemax} IS NULL) ` +
-												`OR (${datemin} IS NULL AND ${datemax} >= CreationDate) ` +
-												`OR (${datemax} IS NULL AND ${datemin} <= CreationDate)) ` +
-							`AND ((Score BETWEEN ${scoremin} AND ${scoremax}) ` +
-										`OR (${scoremin} IS NULL AND ${scoremax} IS NULL) ` +
-										`OR (${scoremin} IS NULL AND ${scoremax} >= Score) ` +
-										`OR (${scoremax} IS NULL AND ${scoremin} <= Score))` +
+							`EXTRACT(YEAR FROM CreationDate) AS year, ` +
+							`EXTRACT(MONTH FROM CreationDate) AS month, ` +
+							`Id, ` +
+							`ParentOrChild, ` +
+							`Score, ` +
+							`${viewstring} ` +
+						`FROM ${table} ` +
+						`WHERE ${qastring}` +
+							`(CreationDate BETWEEN '${datemin}' AND '${datemax}') ` +
+							`AND (Score BETWEEN ${scoremin} AND ${scoremax}) ` +
+							`AND ((PostTypeId = 2) OR (ViewCount BETWEEN ${viewsmin} ` +
+							`AND ${viewsmax}))` +
 							`${titlestring}` +
 							`${bodystring}` +
-							`${tagstring}` +
-							`${viewstring};`;
+							`${tagstring};`;
 	return querystring;
 }
 
 
 
-function checkInjector(bool1, bool2, bool3, type){
-	let outstring = `PostTypeId = ${type}`;
-	if (bool1 == true){
-		if (bool2 == true){
-			if (bool3 == false){
-				outstring += " AND ParentOrChild IS NOT NULL";
-			}
+function checkInjector(json, table){
+	let outstring = ``;
+	if(json.includequestion == true || json.includequestion == json.includeanswer){
+		if(json.includequestion != json.includeanswer){
+			outstring = `(PostTypeId = 1) `;
 		}
-		else{
-			if (bool3 == true){
-				outstring += " AND ParentOrChild IS NULL";
-			}
+		outstring += outstring == `` ? `` : `AND `;
+		if(json.includesatisfied != json.includeunsatisfied){
+			outstring += `(ParentOrChild IS ` + (json.includesatisfied ? `NOT ` : ``) + `NULL) `;
 		}
+		outstring += outstring == `` ? `` : `AND `;
 	}
-	return outstring;
-}
-
-function rangeInjector(boolq, boola, min, max){
-	let outstring = "";
-	if (boolq == true){
-		outstring += ` AND ( ` +
-			`((ViewCount BETWEEN ${min} AND ${max}) ` +
-						`OR (${min} IS NULL AND ${max} IS NULL)) ` +
-						`OR (${min} IS NULL AND ${max} >= ViewCount) ` +
-						`OR (${max} IS NULL AND ${min} <= ViewCount))`;
-		if (boola == true){
-			outstring += " OR (ViewCount IS NULL)";
+	else{
+		outstring = `(PostTypeId = 2) `;
+		if(json.includeaccepted != json.includeother){
+			outstring += `AND (ParentOrChild ` + (json.includeaccepted ? `` :  `NOT `);
+			outstring += `IN (SELECT ParentOrChild From ${table} WHERE PostTypeId = 1)) `;
 		}
+		outstring += `AND `;
 	}
 	return outstring;
 }
 
 function fieldInjector(textarray, field, boolq, boola){
 	let outstring;
-	if ((textarray[0] == "") || ((field == "Title" || field == "Tags") && boolq == false && boola == true)){
-		outstring = ""
+	if((textarray[0] == "") || ((field == "Title" || field == "Tags") && boolq == false)){
+		outstring = ``;
 	}
 	else{
 		outstring = " AND ("
@@ -163,14 +184,61 @@ function fieldInjector(textarray, field, boolq, boola){
 			else{
 				outstring += `(${field} LIKE '% ${textarray[i]} %')`;
 			}
-			if (i+1 != textarray.length){
-				outstring += " AND ";
+			if(i+1 != textarray.length){
+				outstring += ` AND `;
 			}
 		}
-		outstring += ") ";
+		outstring += `)`;
 	}
 	return outstring;
 };
-//app.listen(3000);
-//app.listen(443, err => {if (err) console.log(err);});
-httpssl.listen(443, err => {if (err) console.log(err); });
+
+function viewgroupInjector(){
+	const outstring = `CASE WHEN ViewCOUNT IS NULL THEN NULL ` +
+				`WHEN ViewCount BETWEEN 0 AND 99 THEN 100 ` +
+				`WHEN ViewCount BETWEEN 100 AND 199 THEN 200 ` +
+				`WHEN ViewCount BETWEEN 200 AND 299 THEN 300 ` +
+				`WHEN ViewCount BETWEEN 300 AND 399 THEN 400 ` + 
+				`WHEN ViewCount BETWEEN 400 AND 499 THEN 500 ` +
+				`WHEN ViewCount BETWEEN 500 AND 599 THEN 600 ` +
+				`WHEN ViewCount BETWEEN 600 AND 699 THEN 700 ` +
+				`WHEN ViewCount BETWEEN 700 AND 799 THEN 800 ` +
+				`WHEN ViewCount BETWEEN 800 AND 899 THEN 900 ` +
+				`WHEN ViewCount BETWEEN 900 AND 999 THEN 1000 ` +
+				`WHEN ViewCount BETWEEN 1000 AND 1999 THEN 2000 ` +
+				`WHEN ViewCount BETWEEN 2000 AND 2999 THEN 3000 ` +
+				`WHEN ViewCount BETWEEN 3000 AND 3999 THEN 4000 ` +
+				`WHEN ViewCount BETWEEN 4000 AND 4999 THEN 5000 ` +
+				`WHEN ViewCount BETWEEN 5000 AND 5999 THEN 6000 ` +
+				`WHEN ViewCount BETWEEN 6000 AND 6999 THEN 7000 ` +
+				`WHEN ViewCount BETWEEN 7000 AND 7999 THEN 8000 ` +
+				`WHEN ViewCount BETWEEN 8000 AND 8999 THEN 9000 ` +
+				`WHEN ViewCount BETWEEN 9000 AND 9999 THEN 10000 ` +
+				`WHEN ViewCount BETWEEN 10000 AND 10999 THEN 11000 ` +
+				`WHEN ViewCount BETWEEN 11000 AND 11999 THEN 12000 ` +
+				`WHEN ViewCount BETWEEN 12000 AND 12999 THEN 13000 ` +
+				`WHEN ViewCount BETWEEN 13000 AND 13999 THEN 14000 ` +
+				`WHEN ViewCount BETWEEN 14000 AND 14999 THEN 15000 ` +
+				`WHEN ViewCount BETWEEN 15000 AND 15999 THEN 16000 ` +
+				`WHEN ViewCount BETWEEN 16000 AND 16999 THEN 17000 ` +
+				`WHEN ViewCount BETWEEN 17000 AND 17999 THEN 18000 ` +
+				`WHEN ViewCount BETWEEN 18000 AND 18999 THEN 19000 ` +
+				`WHEN ViewCount BETWEEN 19000 AND 19999 THEN 20000 ` +
+				`WHEN ViewCount BETWEEN 20000 AND 21999 THEN 22000 ` +
+				`WHEN ViewCount BETWEEN 22000 AND 23999 THEN 24000 ` +
+				`WHEN ViewCount BETWEEN 24000 AND 25999 THEN 26000 ` +
+				`WHEN ViewCount BETWEEN 26000 AND 27999 THEN 28000 ` +
+				`WHEN ViewCount BETWEEN 28000 AND 29999 THEN 30000 ` +
+				`WHEN ViewCount BETWEEN 30000 AND 39999 THEN 40000 ` +
+				`WHEN ViewCount BETWEEN 40000 AND 59999 THEN 60000 ` +
+				`WHEN ViewCount BETWEEN 60000 AND 79999 THEN 80000 ` +
+				`WHEN ViewCount BETWEEN 80000 AND 99999 THEN 100000 ` +
+				`WHEN ViewCount BETWEEN 100000 AND 119999 THEN 120000 ` +
+				`WHEN ViewCount BETWEEN 120000 AND 139999 THEN 140000 ` +
+				`WHEN ViewCount BETWEEN 140000 AND 159999 THEN 160000 ` +
+				`WHEN ViewCount BETWEEN 160000 AND 179999 THEN 180000 ` +
+				`WHEN ViewCount BETWEEN 180000 AND 199999 THEN 200000 ` +
+				`WHEN ViewCount BETWEEN 200000 AND 299999 THEN 300000 ` +
+				`ELSE 99999999 END ViewCount`;
+	return outstring;
+}
--- static/src/index.js
+++ static/src/index.js
@@ -1,14 +1,84 @@
 import React, { Component } from 'react';
+import { Chart, CategoryScale, LinearScale, BarElement, PointElement, LineElement, ArcElement, Title,  Tooltip, Legend } from 'chart.js';
+import { Line, Doughnut, Bars } from 'react-chartjs-2';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Search />
-  </React.StrictMode>
-);
+const years = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
+const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const datelabels = [];
+for (const year of years){
+	for (const month of months){
+		datelabels.push(year + '-' + month);
+	}
+}
+Chart.register(CategoryScale, LinearScale, BarElement, ArcElement, PointElement, LineElement, Title, Tooltip, Legend);
+
+const acO = {
+	plugins: {
+		title: {
+			display: true,
+			text: "Number of Answers VS Instances of Each"
+		}
+	}
+};
+
+const vcO = {
+	plugins: {
+		title: {
+			display: true,
+			text: "Number of Views VS Instances of Each"
+		}
+	}
+};
+
+const scO = {
+	plugins: {
+		title: {
+			display: true,
+			text: "Score VS Instances of Each"
+		}
+	}
+};
+
+const uaO = {
+	plugins: {
+		title: {
+			display: true,
+			text: "Unanswered Questions Over Time"
+		}
+	}
+}
+
+const daO = {
+	plugins: {
+		title: {
+			display: true,
+			text: "Posts Over Time"
+		}
+	}
+}
+
+const toO = {
+	plugins: {
+		title: {
+			display: true,
+			text: "Matching Posts"
+		}
+	}
+}
+function populateCharts(thisquery, response){
+	const toD = {
+		//labels,
+		//datasets:
+		//{
+		//data:
+	}
+	return 1;
+}
+
 
 export default class Search extends React.Component {
     constructor(props) {
@@ -29,7 +99,9 @@ export default class Search extends React.Component {
             title: "",
             body: "",
             tags: "", 
-            table: "writers"
+            table: "writers",
+	    advsearch: false,
+	    charts: false
         };
         this.handleSubmit = this.handleSubmit.bind(this);
       }
@@ -53,54 +125,166 @@ export default class Search extends React.Component {
             headers: {
                 'Accept': '*/*',
                 'Content-Type': 'application/json;charset=UTF-8',
-                //'Access-Control-Allow-Origin': '*',
-                //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-                //'Access-Control-Allow-Methods': 'POST',
-                //'Access-Control-Allow-Credentials': true,
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+                'Access-Control-Allow-Methods': 'POST',
+                'Access-Control-Allow-Credentials': true,
             },
             body: JSON.stringify(this.state)
         };
+	const thisquery = this.state;
+	const res =
         await fetch(url, options)
-            .then(response => console.log(response));
+            .then(response => res);
+	console.log(res[0].rows);
+	populateCharts(thisquery, res);
     }
 
     render() {
-        console.log("Hello world: " , this.state);
+        console.log("Current state:" , this.state);
         return (
-            <div style ={{textAlign: 'center'}}>
+	<div>
+	<head>
+		<title>Shufflestack</title>
+	</head>
+	<body style="background-color: #282c34;">
+            <div style={{textAlign: 'center'}}>
                 <div style={{margin: '4em'}} />
-                <h1>ShuffleStack: StackExchange Analytic Search</h1>
+                <h1>Shufflestack: StackExchange Analytic Search</h1>
 			<p>A data tool to facilitate research of the StackExchange Q&A database, best used to compare patterns between its communities and between queries. <br />
             This database is current up to March 7, 2022 and made available through the Internet Archive under a Creative Commons license (CC-BY-SA).<br />
-            As a derivative work, all relevant source code for this tool can be found (here) as required by the license.<br />
+            As a derivative work, all relevant source code for this tool can be found <a href="https://www.github.com/POL0ROID/utsa-shufflestack2">here</a> as required by the license.<br />
             No posts are used directly without attribution. For a more content-centric search, see StackExchange's own search, and consider creating an account to see more than 500 results.</p>
-            <div className="form-container" style={{textAlign: 'left'}}>
+            <div className="form-container">
             	<form className="form" onSubmit={this.handleSubmit}>
+			<input type="checkbox" name="advsearch" id="advsearch" value={this.state.advsearch} onChange={this.handleChange}/><label>Enable Advanced Search</label><br />
+			<hr />
                 	<input type="checkbox" name="includequestion" id="includequestion" value={this.state.isquestion} onChange={this.handleChange}/><label>Questions</label><br />
                     Include:
                      <input type="checkbox" name="includesatisfied" id="includesatisfied" value={this.state.includesatisfied} onChange={this.handleChange}/><label>Satisfied</label>
                      <input type="checkbox" name="includeunsatisfied" id="includeunsatisfied" value={this.state.includeunsatisfied} onChange={this.handleChange}/><label>Unsatisfied</label><br />
-                     <input type="text" name="viewsmin" id="viewsmin" size="2" value={this.state.viewsmin} onChange={this.handleChange}/><label>Minimum Views</label><br />
-                     <input type="text" name="viewsmax" id="viewsmax" size="2" value={this.state.viewsmax} onChange={this.handleChange}/><label>Maximum Views</label><br />
+                     <input type="text" name="viewsmin" id="viewsmin" value={this.state.viewsmin} onChange={this.handleChange}/><label>Minimum Views</label><br />
+                     <input type="text" name="viewsmax" id="viewsmax" value={this.state.viewsmax} onChange={this.handleChange}/><label>Maximum Views</label><br />
                      <textarea name="tags" id="tags" rows="1" value={this.state.tags} onChange={this.handleChange}/><label>Tags</label>< br/>
                      <textarea name="title" id="title" rows="1" value={this.state.title} onChange={this.handleChange}/><label>Title</label>< br/>
                     <hr />
                     <input type="checkbox" name="includeanswer" id="includeanswer" value={this.state.includeanswer} onChange={this.handleChange}/><label>Answers</label><br />
                     Include:
                     <input type="checkbox" name="includeaccepted" id="includeaccepted" value={this.state.includeaccepted} onChange={this.handleChange}/><label>Accepted</label>
-                    <input type="checkbox" name="includeother" value={this.state.includeother} onChange={this.handleChange}/><label>Other</label><br />
+                    <input type="checkbox" name="includeother" id="includeother" value={this.state.includeother} onChange={this.handleChange}/><label>Other</label><br />
                     <hr />
                     Minimum:<input type="datetime-local" name="datemin" id="adatemin" value={this.state.datemin} onChange={this.handleChange}/><label>Date </label>
-                            <input type="text" name="scoremin" id="ascoremin" size="2" value={this.state.scoremin} onChange={this.handleChange}/><label>Score</label><br />
+                            <input type="text" name="scoremin" id="scoremin" value={this.state.scoremin} onChange={this.handleChange}/><label>Score</label><br />
                     Maximum:<input type="datetime-local" name="datemax" id="adatemax" value={this.state.datemax} onChange={this.handleChange}/><label>Date </label>
-                            <input type="text" name="scoremax" id="ascoremax" size="2" value={this.state.scoremax} onChange={this.handleChange}/><label>Score</label><br />
+                           <input type="text" name="scoremax" id="scoremax" value={this.state.scoremax} onChange={this.handleChange}/><label>Score</label><br />
                     <textarea name="body" id="body" rows="1" value={this.state.body} onChange={this.handleChange}/><label>Body</label>< br/>
-                    <select name="table" id="table" value={this.state.table} onChange={this.handleChange}><option value="stackoverflow"/><option value="writing"/></select><label>Category</label><br />
+                    <div><select name="table" id="table" value={this.state.table} onChange={this.handleChange} required>
+		<optgroup label="Main"><option value="stackoverflow">Stack Overflow (very slow)</option>
+		<option value="stackexchange">Stack Exchange</option></optgroup>
+		<option value="academia">Academia</option>
+		<option value="ai">AI</option>
+		<option value="ham">Amateur Radio</option>
+		<option value="android">Android Enthusiasts</option>
+		<option value="anime">Anime & Manga</option>
+		<option value="arduino">Arduino</option>
+		<option value="gaming">Arqade</option>
+		<option value="crafts">Arts & Crafts</option>
+		<option value="apple">Ask Different</option>
+		<option value="patents">Ask Patents</option>
+		<option value="askubuntu">Ask Ubuntu</option>
+		<option value="astronomy">Astronomy</option>
+		<option value="aviation">Aviation</option>
+		<option value="alcohol">Beer, Wine & Spirits</option>
+		<option value="hermeneutics">Biblical Hermeneutics</option>
+		<option value="biology">Biology</option>
+		<option value="blender">Blender</option>
+		<option value="boardgames">Board & Card Games</option>
+		<option value="chemistry">Chemistry</option>
+		<option value="chess">Chess</option>
+		<option value="chinese">Chinese Language</option>
+		<option value="christianity">Christianity</option>
+		<option value="codegolf">Code Golf</option>
+		<option value="codereview">Code Review</option>
+		<option value="coffee">Coffee</option>
+		<option value="cogsci">CogSci</option>
+		<option value="scicomp">Computational Science</option>
+		<option value="cseducators">Computer Science Educators</option>
+		<option value="cs">Computer Science</option>
+		<option value="cstheory">Theoretical Computer Science</option>
+		<option value="craftcms">Craft CMS</option>
+		<option value="stats">Cross Validated</option>
+		<option value="crypto">Cryptography</option>
+		<option value="datascience">Data Science</option>
+		<option value="dba">Database Administrators</option>
+		<option value="devops">DevOps</option>
+		<option value="drones">Drones and Model Aircraft</option>
+		<option value="drupal">Drupal Answers</option>
+		<option value="earthscience">Earth Science</option>
+		<option value="ebooks">Ebooks</option>
+		<option value="economics">Economics</option>
+		<option value="electronics">Electric Engineering</option>
+		<option value="engineering">Engineering</option>
+		<option value="english">English Language & Usage</option>
+		<option value="ell">English Language Learners</option>
+		<option value="ethereum">Ethereum</option>
+		<option value="french">French Language</option>
+		<option value="gamedev">Game Development</option>
+		<option value="gardening">Gardening & Landscaping</option>
+		<option value="gis">Geographic and Information Systems</option>
+		<option value="graphicdesign">Graphic Design</option>
+		<option value="diy">Home Improvement</option>
+		<option value="interpersonal">Interpersonal Skills</option>
+		<option value="lifehacks">Lifehacks</option>
+		<option value="linguistics">Linguistics</option>
+		<option value="magento">Magento</option>
+		<option value="mathoverflow">MathOverflow</option>
+		<option value="mathematica">Mathematica</option>
+		<option value="math">Mathematics (slow)</option>
+		<option value="judaism">Mi Yodeya</option>
+		<option value="music">Music</option>
+		<option value="outdoors">The Great Outdoors</option>
+		<option value="parenting">Parenting</option>
+		<option value="money">Personal Finance and Money</option>
+		<option value="philosophy">Philosophy</option>
+		<option value="photo">Photography</option>
+		<option value="fitness">Physical Fitness</option>
+		<option value="physics">Physics</option>
+		<option value="puzzling">Puzzling</option>
+		<option value="raspberrypi">Raspberry Pi</option>
+		<option value="retrocomputing">Retrocomputing</option>
+		<option value="robotics">Robotics</option>
+		<option value="rpg">Role-playing Games</option>
+		<option value="salesforce">Salesforce</option>
+		<option value="scifi">Science Fiction & Fantasy</option>
+		<option value="serverfault">Server Fault</option>
+		<option value="sharepoint">Sharepoint</option>
+		<option value="sitecore">Sitecore</option>
+		<option value="softwareengineering">Software Engineering</option>
+		<option value="softwarerecs">Software Recommendations</option>
+		<option value="sqa">Software Quality Assurance & Testing</option>
+		<option value="spanish">Spanish Language</option>
+		<option value="superuser">Superuser</option>
+		<option value="tex">TeX - LaTeX</option>
+		<option value="travel">Travel</option>
+		<option value="unix">Unix & Linux</option>
+		<option value="ux">User Experience</option>
+		<option value="vi">Vi and Vim</option>
+		<option value="webmasters">Webmasters</option>
+		<option value="wordpress">WordPress Development</option>
+		<option value="workplace">The Workplace</option>
+		<option value="worldbuilding">Worldbuilding</option>
+		<option value="writers">Writers</option>
+		</select>
+		</div>
+		<label>Category</label><br />
+		<Doughnut options={toO} /><br />
                     <input type="submit" name="submit" id="submit" value="Submit"></input>
                 </form>
             </div>
             </div>
-        )
+	</body>
+	</div>
+        );
     }
 }
 
@@ -108,3 +292,9 @@ export default class Search extends React.Component {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Search />
+  </React.StrictMode>
+);
